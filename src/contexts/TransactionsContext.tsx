6e6756c3645520dee94cdf6f285465cfcd093e01
Lincoln <jeanlincol@gmail.com>
2023-01-09 21:18:43 -0300

    json server added

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,102 +1,75 @@
-import { ReactNode, createContext, useState } from 'react';
+import { api } from '../../axios'
+import { ReactNode, createContext, useState, useEffect } from 'react'
 
 type IncomeTransacion = {
-  id: string;
-  data: string;
-  description: string;
-  origin: string;
-  value: number;
-};
+  id: string
+  data: string
+  description: string
+  origin: string
+  value: number
+}
 
 type OutcomeTransaction = {
-  id: string;
-  data: string;
-  description: string;
-  method: string;
-  type: string;
-  value: number;
-};
+  id: string
+  data: string
+  description: string
+  method: string
+  type: string
+  value: number
+}
 
 type TransactionContextType = {
-  incomeValues: IncomeTransacion[];
-  outcomeValues: OutcomeTransaction[];
-  incomeTotal: () => string;
-  outcomeTotal: () => string;
-};
+  incomeValues: IncomeTransacion[]
+  outcomeValues: OutcomeTransaction[]
+  incomeTotal: () => string
+  outcomeTotal: () => string
+}
 
 type CyclesContextProviderProps = {
-  children: ReactNode;
-};
+  children: ReactNode
+}
 
-export const TransactionsContext = createContext({} as TransactionContextType);
+export const TransactionsContext = createContext({} as TransactionContextType)
 
-export function TransactionsContextProvider({ children }: CyclesContextProviderProps) {
-  const [incomeValues, setIncomeValues] = useState<IncomeTransacion[]>([
-    {
-      id: '1',
-      data: '10/11/2022',
-      description: 'Salario',
-      origin: 'Unicred',
-      value: 2000,
-    },
-    {
-      id: '2',
-      data: '11/11/2022',
-      description: 'FreeLancer',
-      origin: 'FreeLancer',
-      value: 1000,
-    },
-    {
-      id: '3',
-      data: '12/11/2022',
-      description: 'Bico',
-      origin: 'LGPD',
-      value: 50,
-    },
-  ]);
+export function TransactionsContextProvider({
+  children,
+}: CyclesContextProviderProps) {
+  const [incomeValues, setIncomeValues] = useState<IncomeTransacion[]>([])
 
-  const [outcomeValues, setOutcomeValues] = useState<OutcomeTransaction[]>([
-    {
-      id: '1',
-      data: '10/11/2022',
-      description: 'Hamburguer',
-      method: 'Pix',
-      type: 'Comida',
-      value: 30,
-    },
-    {
-      id: '2',
-      data: '11/11/2022',
-      description: 'No Mans sky',
-      method: 'Credit Card',
-      type: 'Jogo',
-      value: 100,
-    },
-    {
-      id: '3',
-      data: '12/11/2022',
-      description: 'Ryzen 5 5500',
-      method: 'Credit Card',
-      type: 'Eletronic',
-      value: 1200,
-    },
-  ]);
+  const [outcomeValues, setOutcomeValues] = useState<OutcomeTransaction[]>([])
+
+  const fetchTransactions = async () => {
+    const incomeResponse = await api.get('incomeTransactions')
+    const outcomeResponse = await api.get('outcomeTransactions')
+    setIncomeValues(incomeResponse.data)
+    setOutcomeValues(outcomeResponse.data)
+  }
+
+  useEffect(() => {
+    fetchTransactions()
+  }, [])
 
   const incomeTotal = () => {
     const total = incomeValues.reduce((acc, income) => {
-      acc = acc += income.value;
-      return acc;
-    }, 0);
-    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(total);
-  };
+      acc = acc += income.value
+      return acc
+    }, 0)
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(total)
+  }
 
   const outcomeTotal = () => {
     const total = outcomeValues.reduce((acc, income) => {
-      acc = acc += income.value;
-      return acc;
-    }, 0);
-    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(total);
-  };
+      acc = acc += income.value
+      return acc
+    }, 0)
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(total)
+  }
 
   return (
     <TransactionsContext.Provider
@@ -109,5 +82,5 @@ export function TransactionsContextProvider({ children }: CyclesContextProviderP
     >
       {children}
     </TransactionsContext.Provider>
-  );
+  )
 }
