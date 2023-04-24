1a8bf5073cc1cd3c2f09da9379f82e7feb7ef9e4
Lincoln <jeanlincol@gmail.com>
2023-04-24 19:22:57 -0300

    Loading component bugfix

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,4 +1,4 @@
-import * as S from '../../styles/components/loading'
+import * as S from '../../styles/components/Loading'
 
 export function Loading() {
   return <S.Loader className="loader"></S.Loader>
